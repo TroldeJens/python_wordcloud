4ac194e5950149a9c03f00bf04aa57f9ee55cd54
TroldeJens <jens.laursen@lego.com>
2023-02-06 14:32:35 +0100

    feat: Major update: 1) Add additional mask-handling so it can handle transparent background etc. 2) Ensured that resulting image is saved with the target width and length 3) Refactoring

--- create_wordcloud.py
+++ create_wordcloud.py
@@ -1,21 +1,25 @@
 ### Globals
-# Input
-inputFilename                       = "input.txt"
+# Input and output
+input_filename                      = "input.txt"
+result_filename                     = "resulting_wordcloud.png"
+
+# Adjust text
 lowercase_everything                = False         # Make all strings lowercase.
 uppercase_everything                = False         # Make all strings uppercase.
-start_all_words_with_capital_letter = True          # Start every word with a capital letter. Can be combined with lowercase_everything
+start_all_words_with_capital_letter = True          # Start every word with a capital letter. Can be combined with lowercase_everything.
 
 # Customize wordcloud
-background_colour                   = "white"
 width                               = 1000
 height                              = 500
+make_background_transparent         = False         # If True, the background will be transparent. If no, use the colour defined below.
+background_colour                   = "white"
 use_custom_font                     = False         # If True, use the font defined below.
 custom_font_path                    = "fonts/Roboto/Roboto-Italic.ttf"
 use_custom_mask                     = True          # If True, use the image defined below as a mask. White is ignored. Everything else becomes the form.
-custom_mask_path                    = "mask_images/square.png"
+custom_mask_path                    = "mask_images/arrow_left.png"
 
 # Other
-debug                               = True
+debug                               = False
 
 import os
 from os import path
@@ -36,15 +40,32 @@ def capitalize_first_letter_of_every_word(text: str) -> str:
         words.append(capitalize_first_letter(word))
     return ' '.join(words)
 
+def cleanup_and_adjust_mask_image(mask: Image.Image) -> Image.Image:
+    """Cleanup the mask image. 
+        1) Resize to fit target width and height
+        2) Convert pixel values 
+        3) Ensure that transparent pixels become white
+    """
+    # Resize the image to fit the target width and height.
+    #   If we don't do this, then the target width and height will be ignored and the result will use the shape of the mask image instead. 
+    mask    = mask.resize((width, height))
+    # Ensure that white pixels have the value 0-255, and not 0-1.
+    #   We use RGBA here, because if you use RGB, it makes all transparent background black, which is not what we want.
+    mask    = mask.convert("RGBA")
+    # Ensure that transparent background is coloured white.
+    tmpMask = Image.new("RGBA", mask.size, "white") # Create a new image with a white rgba background.
+    tmpMask.paste(mask, mask)                       # Paste the image onto the background.
+    return tmpMask.convert("RGB")                   # Return a 24bit version of the tmpMask image.
+
 def generate_wordcloud():
     """ Generate wordcloud using the global variables defined in the beginning of the document."""
 
-    # Get data directory (using getcwd() is needed to support running example in generated IPython notebook)
+    # Get data directory (using getcwd() is needed to support running example in generated IPython notebook).
     d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
 
-    # Read file and add each line to a list
+    # Read file and add each line to a list.
     lines = []
-    with open(path.join(d, inputFilename)) as fileinput:
+    with open(path.join(d, input_filename)) as fileinput:
         lines = fileinput.read().splitlines()
 
     if lowercase_everything:
@@ -53,7 +74,7 @@ def generate_wordcloud():
     if uppercase_everything:
         lines = [line.upper() for line in lines]
 
-    if start_all_words_with_capital_letter:
+    if start_all_words_with_capital_letter and not uppercase_everything:
         lines = [capitalize_first_letter_of_every_word(line) for line in lines]
 
     # Count each unique line
@@ -69,33 +90,28 @@ def generate_wordcloud():
         print(word_count_dictionary)
         print("---DEBUG END---")
 
-    # setup wordcount
+    # Setup WordCloud
     wordcloud = WordCloud()
+    wordcloud.mode              = "RGBA"
     wordcloud.width             = width
     wordcloud.height            = height
-    wordcloud.background_color  = background_colour
+    wordcloud.background_color  = None if make_background_transparent else background_colour
     if use_custom_font:
         wordcloud.font_path     = custom_font_path
     if use_custom_mask:
-        # Open the mask image and resize it to fit the target width and height.
-        #   If we don't do this, then the target width and height will be ignored and the result will use the shape of the mask image instead. 
-        mask = Image.open(custom_mask_path).resize((width, height))
-        # Ensure that white pixels have the value 0-255, and not 0-1
-        mask = mask.convert("RGB")
-        wordcloud.mask = np.array(mask)
+        mask                    = Image.open(custom_mask_path)
+        mask                    = cleanup_and_adjust_mask_image(mask)
+        wordcloud.mask          = np.array(mask)
         
-    # generate wordcount
+    # Generate wordcount
     wordcloud.generate_from_frequencies(word_count_dictionary)
 
-    plt.figure(figsize=(15,8))
-    plt.imshow(wordcloud)
     plt.axis("off")
-    plt.savefig("resulting_wordcloud"+".png", bbox_inches='tight')
-    plt.show()
-    plt.close()
+    plt.imsave(fname=result_filename, arr=np.array(wordcloud), format="png") # Use imsave in order to save image in the exact size as defined by width and height.
 
     return
 
 # Run program
 if __name__ == '__main__':
     generate_wordcloud()
+    print("WordCloud image has been generated. Resulting image: ", result_filename)
